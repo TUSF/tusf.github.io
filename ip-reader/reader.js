9789ab31a98185e566e03ff620ee9f7e23f136e2
TUSF <ragef33@gmail.com>
2018-01-08 06:52:42 -0600

    Load the image as well.

--- ip-reader/reader.js
+++ ip-reader/reader.js
@@ -1,13 +1,29 @@
 function getIndex(hash, callback) {
-	if( typeof hash === "string" ) {
+	if( typeof hash === "string" && typeof callback === "function" ) {
 		node.files.cat(hash + "/index.json", function(err, data) {
 			if(err) {
-				return console.error('Error - ipfs files cat', err, res)
+				return console.error('Error - ipfs files cat', err)
 			}
+			index = JSON.parse(data)
 
-			if( typeof callback === "function" ) {
-				callback(data)
-			}
-	})
+			node.files.cat(hash + "/" + index.cover, function(err, idata) {
+				if(err) {
+					return console.error('Error - ipfs files cat', err)
+				}
+				mediatype = "image/"
+				switch ( index.cover.substr(index.cover.lastIndexOf('.') + 1) ) {
+					case 'png':
+						mediatype += "png"
+					case 'gif':
+						mediatype += "gif"
+					case 'jpg':
+					case 'jpeg':
+						mediatype += "jpeg"
+				}
+
+				index.coverdata = "data:" + mediatype + ";base64," + idata.toString('base64')
+				callback(index)
+			})
+		})
 	}
 }
